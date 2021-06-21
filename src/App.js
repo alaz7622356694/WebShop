b83e1f518940ca24f8f31fc5de8e79b6e72f5179
alaz7622356694 <alireza.azari2235@outlook.com>
2021-06-21 19:45:48 +0430

    Adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './components/Haeader/Header'
 import Footer from './components/Footer/Footer'
+import {Container} from 'react-bootstrap'
 const App = () => {
     return (
         <div>
           <Header/>
+          <Container>
+              <h2>FC Market</h2>
+          </Container>
           <Footer/>
         </div>
     )
